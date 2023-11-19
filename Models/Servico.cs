a7fdc34f4e9de788548789b21a49fc519ce1efd9
Kleverton-Patrick <augusto.omena.n@gmail.com>
2023-11-19 11:53:41 -0300

    Add project files.

---
+++ Models/Servico.cs
@@ -0,0 +1,15 @@
+﻿namespace AspNetCoreWebApp.Models
+{
+    public class Servico
+    {
+        public int IdServico { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int Estoque { get; set; }
+    }
+}
